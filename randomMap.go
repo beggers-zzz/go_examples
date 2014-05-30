5b6e4c1e6041cb1977eca29cc3327bdd815db860
Ben Eggers <beggerss@cs.washington.edu>
2014-05-30 15:00:35 -0700

    added random map

---
+++ randomMap.go
@@ -0,0 +1,25 @@
+// Ben Eggers
+// GNU GPL'd
+
+// Demonstrates random map iteration in Go (proving it to myself)
+
+package main
+
+import "fmt"
+
+func main() {
+
+	things := map[rune]int{} // our demo map
+	numChars := 10           // number of characters to print
+	initChar := 60           // character to start on
+
+	// populate
+	for i := initChar; i < initChar+numChars; i++ {
+		things[rune(i)] = i
+	}
+
+	// and print
+	for character, charCount := range things {
+		fmt.Println(string(character), ":", charCount)
+	}
+}
